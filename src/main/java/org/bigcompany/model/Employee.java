af34798b3e73fa159d8af7eca8ab7442212b3f5f
Marinas Claudiu <marinasclaudiu@Marinass-MacBook-Pro-2.local>
2024-04-06 21:27:41 +0100

    Remove lombok

--- src/main/java/org/bigcompany/model/Employee.java
+++ src/main/java/org/bigcompany/model/Employee.java
@@ -1,23 +1,74 @@
 package org.bigcompany.model;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 
-@Data
-@RequiredArgsConstructor
 public class Employee {
 
-    private final Long id;
+    private Long id;
 
-    private final String firstName;
+    private String firstName;
 
-    private final String lastName;
+    private String lastName;
 
-    private final Long salary;
+    private Long salary;
 
-    private final Long managerId;
+    private Long managerId;
 
     private List<Employee> subordinates;
+
+    public Employee(Long id, String firstName, String lastName, Long salary, Long managerId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.managerId = managerId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(List<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
 }
